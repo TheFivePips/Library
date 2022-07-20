5f41dcb9692702bd9ec3437f77e9bbb936d8f97e
sam gardner <samgardner20@gmail.com>
2022-07-20 16:21:57 -0400

    Stopping for today. still trying to figure out the read-status button

--- app.js
+++ app.js
@@ -1,35 +1,87 @@
 
-
 const cards = document.querySelector('.cards')
 const form = document.getElementById("book-form")
-// const addBookBtn = document.getElementById("submit-btn")
 
-
-
-const  myLibrary = [
+let  myLibrary = [
     {
-        id: 0,
-        title: 'The Left Hand of Darkness',
-        author: 'Ursula K. Le Guin',
-        pages: 286,
-        read: 'Not-Read'
-    }, 
-    
+    "author": "Ursula",
+    "id": 0,
+    "pages": 295,
+    "read": "Not-Read",
+    "title": "The Left Hand of Darkness"
+    }
 ]
-myLibrary.forEach(element => {
+
+// function showWholeLibrary(){
+//     for(let i= 0; i <myLibrary.length; i++){
+//         const card = document.createElement('div')
+//         card.classList.add("card")
+//         card.setAttribute("id", myLibrary[i])
+//         card.innerHTML = `
+//             <h3>${lastItem.title}</h3>
+//             <p>Author: <span>${lastItem.author}</span></p>
+//             <p>Pages: <span>${lastItem.pages}</span></p>
+//             <p>Read-status: <span id="read-status">${lastItem.read}</span></p>
+//             <button id=${lastItem.id} type="input">Change Read-status</button>
+//             <button id=${lastItem.id}>Remove</button>`
+    
+//     }
+// }
+
+function displayNewCard(){
+    const lastItem = myLibrary[myLibrary.length-1]
+
     const card = document.createElement('div')
     card.classList.add("card")
+    card.setAttribute('id', myLibrary.indexOf(lastItem))
     card.innerHTML = `
-        <h3>${element.title}</h3>
-        <p>Author: <span>${element.author}</span></p>
-        <p>Pages: <span>${element.pages}</span></p>
-        <p>Read-status: <span>${element.read}</span></p>
-        <button type="input">Change Read-status</button>
-        <button id=${element.id}>Remove</button>`
+        <h3>${lastItem.title}</h3>
+        <p>Author: <span>${lastItem.author}</span></p>
+        <p>Pages: <span>${lastItem.pages}</span></p>
+        <p>Read-status: <span id="read-status">${lastItem.read}</span></p>
+        <button id=${lastItem.id} type="input">Change Read-status</button>
+        <button id=${lastItem.id}>Remove</button>`
 
+    card.addEventListener('click', function(event) {
+        let btn = event.target
+        
+        if(btn.innerHTML === "Remove"){
+            for(let i =0; i< myLibrary.length; i++){
+                if(btn.id === myLibrary[i].id) {
+                    myLibrary.splice(btn.id,1)
+                }
+                cards.removeChild(card)
+            }
+        }
+    })
+
+    const read = card.childNodes[7]
+    console.log(read)
+        // if(btn.innerHTML === "Change Read-status") {
+        //     const statusSpan = document.getElementById("read-status")
+        //     console.log(card.id)
+        //     console.log(card.statusSpan.innerHTML)
+            
+        //     const span = document.getElementById("read-status")
+        //    for(let i =0; i < myLibrary.length; i++){
+        //     if(btn.id === myLibrary[i].id){
+        //        if(myLibrary[i].read === "Read"){
+        //         myLibrary[i].read = "Not-Read"
+        //         span.innerHTML = myLibrary[i].read
+        //        }
+        //        if(myLibrary[i].read === "Not-Read"){
+        //         myLibrary[i].read = "Read"
+        //         span.innerHTML = myLibrary[i].read
+
+        //        }
+        //     }
+        //    }
+
+        
+      
+          
     cards.appendChild(card)
-
-})
+}
 
 class Book {
     constructor (id, title, author, pages, read){ 
@@ -39,18 +91,21 @@ class Book {
         this.pages = pages
         this.read = read
     }
-
-    info() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    toggleRead(id){
+        if(id === this.id){
+            if(this.read === "Read"){
+                this.read === "Not-Read"
+            }
+            if(this.read === "Not-Read"){
+                this.read === "Read"
+            }
     }
-
+    }
 }
-
 
 form.addEventListener("submit", function(e) {
     e.preventDefault()
-    // console.log(document.getElementById("book-title").value)
-   
+    
     let book = new Book(
         myLibrary.length,
         document.getElementById("book-title").value,
@@ -59,44 +114,7 @@ form.addEventListener("submit", function(e) {
         document.querySelector('input[name="radio"]:checked').value
     )
     myLibrary.push(book)
-
-    const card = document.createElement('div')
-    card.classList.add("card")
-    
-    card.innerHTML = `
-    <h3>${book.title}</h3>
-    <p>Author: <span>${book.author}</span></p>
-    <p>Pages: <span>${book.pages}</span></p>
-    <p>Read-status: <span id="${book.id}R">${book.read}</span></p>
-    <button type="input">Change Read-status</button>
-    
-    <button id=${book.id}>Remove</button>`;
-
-    card.addEventListener('click', function(event) {
-        let btn = event.target
-        // console.log(btn.innerHTML)
-        if(btn.id && btn.innerHTML === "Remove"){
-            for(let i =0; i< myLibrary.length; i++){
-                if(btn.id === myLibrary[i].id) {
-                    myLibrary.splice(btn.id,1)
-                }
-                cards.removeChild(card)
-            }
-        }
-        const statusSpan = document.getElementById(`${book.id}R`)
-        if(btn.innerHTML === "Change Read-status"){
-        //    console.log(statusSpan.innerHTML)
-            if(statusSpan.innerHTML === "Read"){
-                statusSpan.innerHTML = "Not-Read"
-            }
-            else if(statusSpan.innerHTML === "Not-Read"){
-                statusSpan.innerHTML = "Read"
-            }
-            
-        }
-    })
-  
-    cards.appendChild(card)
+    displayNewCard()
    
 });
 

71c0461de11d87bb6e37d83c3d5dcba9fefaa18f
sam gardner <samgardner20@gmail.com>
2022-07-21 15:22:51 -0400

    This is finally working fully. all basic functionality is done. just need to add some styling

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
+
 const cards = document.querySelector(".cards")
 const myForm = document.getElementById('book-form')
 
-console.log(myForm)
 
 let  myLibrary = [
-    {
-    "title": "The Left Hand of Darkness",
-    "author": "Ursula",
-    "id": 1,
-    "pages": 295,
-    "read": false
-    },
-    {
-    "title": "The Hobbit",
-    "author": "Tolkien",
-    "id": 2,
-    "pages": 295,
-    "read": true
-    },
-    {
-    "title": "The Lies of Locke Lamora",
-    "author": "Scott Lynch",
-    "id": 3,
-    "pages": 295,
-    "read": true
-    }
+    // {
+    // "title": "The Left Hand of Darkness",
+    // "author": "Ursula",
+    // "id": 0,
+    // "pages": 295,
+    // "read": false
+    // },
+    // {
+    // "title": "The Hobbit",
+    // "author": "Tolkien",
+    // "id": 1,
+    // "pages": 295,
+    // "read": true
+    // },
+    // {
+    // "title": "The Lies of Locke Lamora",
+    // "author": "Scott Lynch",
+    // "id": 2,
+    // "pages": 295,
+    // "read": true
+    // }
 ]
 
 // display the whole library, just to see what were workign with
@@ -38,6 +38,10 @@ function Book(title, author, id, pages, read) {
     this.pages = pages
     this.read = read
 
+}
+
+Book.prototype.toggleRead = function(){
+    this.read = !this.read
 }
 
 
@@ -60,12 +64,10 @@ const formSubmitBtn = document.getElementById('submit-btn')
 formSubmitBtn.addEventListener("click", function(event) {
     event.preventDefault()
     add_to_myLibrary()
-    // display_library()
-    display_new_book()
+    cards.replaceChildren()
+    display_library()
+    // display_new_book()
 })
-
-// Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
-
 
 
 // this will dipslay the entire library everytime. need to remove old displays or write a new fucntion that only adds the latest book (see function display_new_book)
@@ -81,7 +83,7 @@ function display_library(){
 
         const card = document.createElement("div")
         card.setAttribute("class", "card")
-
+        card.setAttribute("id", i)
         const bookTitle = document.createElement("p")
         bookTitle.setAttribute("class", "book-title")
         bookTitle.innerText = title
@@ -103,11 +105,39 @@ function display_library(){
         removeBtn.type = "button"
         removeBtn.innerText = "Remove"
 
+        removeBtn.addEventListener("click", function() {
+            // loop throu library and remove the book at the given index and then remove that card from the cards page
+            for(let i=0; i < myLibrary.length; i++){
+                myLibrary.splice(removeBtn.dataset.id,1)
+                cards.removeChild(card)
+    
+            }
+          
+        })
+        const readBtn = document.createElement("button")
+        readBtn.setAttribute("class", "read-status")
+        readBtn.type = "button"
+        readBtn.innerText = "Change Read Status"
+    // update the specific item, remove the nodes from the cards page and re render the entire library
+        readBtn.addEventListener("click", function(event) {
+            event.preventDefault()
+            const id = event.target.parentElement.id
+            // console.log(event.target.parentElement.id)
+            myLibrary[id].toggleRead()
+            cards.replaceChildren()
+            // console.log(myLibrary)
+            // console.log(cards)
+            display_library()
+    })
+    
+
+
 
         card.append(bookTitle)
         card.append(bookAuthor)
         card.append(bookPages)
         card.append(bookRead)
+        card.append(readBtn)
         card.append(removeBtn)
 
         cards.append(card)
@@ -115,80 +145,93 @@ function display_library(){
 
     }
 }
-
-function display_new_book(){
+// takes the last elelment of the library array and makes a card and appends to the page
+// function display_new_book(){
     
-    let latestBook = myLibrary[myLibrary.length-1]
+//     let latestBook = myLibrary[myLibrary.length-1]
 
-    let title = latestBook.title
-    let author = latestBook.author
-    let pages = latestBook.pages
-    let read = latestBook.read
+//     let title = latestBook.title
+//     let author = latestBook.author
+//     let pages = latestBook.pages
+//     let read = latestBook.read
 
-    const card = document.createElement("div")
-    card.setAttribute("class", "card")
+//     const card = document.createElement("div")
+//     card.setAttribute("class", "card")
+//     card.setAttribute("id", myLibrary.indexOf(latestBook))
 
-    const bookTitle = document.createElement("p")
-    bookTitle.setAttribute("class", "book-title")
-    bookTitle.innerText = title
+//     const bookTitle = document.createElement("p")
+//     bookTitle.setAttribute("class", "book-title")
+//     bookTitle.innerText = title
 
-    const bookAuthor = document.createElement("p")
-    bookAuthor.setAttribute("class", "book-author")
-    bookAuthor.innerText = author
+//     const bookAuthor = document.createElement("p")
+//     bookAuthor.setAttribute("class", "book-author")
+//     bookAuthor.innerText = author
 
-    const bookPages = document.createElement("p")
-    bookPages.setAttribute("class", "book-pages")
-    bookPages.innerText = `Pages: ${pages}`
+//     const bookPages = document.createElement("p")
+//     bookPages.setAttribute("class", "book-pages")
+//     bookPages.innerText = `Pages: ${pages}`
 
-    const bookRead = document.createElement("p")
-    bookRead.setAttribute("class", "book-read")
-    bookRead.innerText = `Read: ${read}`
-
-    const removeBtn = document.createElement("button")
-    removeBtn.setAttribute("data-id", myLibrary.indexOf(latestBook))
-    removeBtn.type = "button"
-    removeBtn.innerText = "Remove"
+//     let bookRead = document.createElement("p")
+//     bookRead.setAttribute("id", "book-read")
     
-    //attach an event listener onto the remove btns that will remove it from the library and then remove it from the page
+//     bookRead.innerText = `Read: ${read}`
 
-    removeBtn.addEventListener("click", function() {
-        // loop throu library and remove the book at the given index and then remove that card from the cards page
-        for(let i=0; i < myLibrary.length; i++){
-            myLibrary.splice(removeBtn.dataset.id,1)
-            cards.removeChild(card)
-        }
+//     const removeBtn = document.createElement("button")
+//     removeBtn.setAttribute("data-id", myLibrary.indexOf(latestBook))
+//     removeBtn.type = "button"
+//     removeBtn.innerText = "Remove"
+    
+//     //attach an event listener onto the remove btns that will remove it from the library and then remove it from the page
+
+//     removeBtn.addEventListener("click", function() {
+//         // loop throu library and remove the book at the given index and then remove that card from the cards page
+//         for(let i=0; i < myLibrary.length; i++){
+//             myLibrary.splice(removeBtn.dataset.id,1)
+//             cards.removeChild(card)
+
+//         }
       
-    })
-// Add a button on each book’s display to change its read status.
-// To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
+//     })
+// // Add a button on each book’s display to change its read status.
+// // To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
 
-    const readBtn = document.createElement("button")
-    readBtn.setAttribute("class", "read-status")
-    readBtn.type = "button"
-    readBtn.innerText = "Change Read Status"
+//     const readBtn = document.createElement("button")
+//     readBtn.setAttribute("class", "read-status")
+//     readBtn.type = "button"
+//     readBtn.innerText = "Change Read Status"
+
+//     // update the specific item, remove the nodes from the cards page and re render the entire library
+//     readBtn.addEventListener("click", function(event) {
+        
+//         const id = myLibrary.indexOf(latestBook)
+//         myLibrary[id].toggleRead()
+//         cards.replaceChildren()
+//         display_library()
+//     })
     
 
-    readBtn.addEventListener("click", function() {
-        console.log("clicked")
-    })
-
-
-    card.append(bookTitle)
-    card.append(bookAuthor)
-    card.append(bookPages)
-    card.append(bookRead)
-    card.append(readBtn)
-    card.append(removeBtn)
-
+    
+    
+//     card.append(bookTitle)
+//     card.append(bookAuthor)
+//     card.append(bookPages)
+//     card.append(bookRead)
+//     card.append(readBtn)
+//     card.append(removeBtn)
+    
         
 
-    cards.append(card)
+//     cards.append(card)
 
 
     
-}
+// }
+//  function updateReadStatus(id){
+//     myLibrary[id].toggleRead()
+    
 
-// Add a button on each book’s display to remove the book from the library.
-//You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 
 
+//  }
+
+// console.log(myLibrary)
